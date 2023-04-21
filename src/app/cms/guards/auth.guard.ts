0b1b7712b8f6158c2180f92a1b14a84a29a786b9
Juan Esteban Villegas Arias <estebanari29@gmail.com>
2023-04-20 22:16:40 -0500

    feat: better implementation of auth guard

--- src/app/cms/guards/auth.guard.ts
+++ src/app/cms/guards/auth.guard.ts
@@ -1,13 +1,15 @@
 import { inject } from '@angular/core';
+import { Router } from '@angular/router';
 import { Store } from '@ngrx/store';
 import { selectAuthorizationToken } from 'src/app/state/selectors/admin.selector';
 
 export const authGuard = () => {
   const store = inject(Store);
+  const router = inject(Router);
   let hasToken = false;
   store
     .select(selectAuthorizationToken)
     .forEach((token) => (hasToken = !!token));
 
-  return hasToken;
+  return hasToken ? true : router.navigate(['/cms']);
 };
